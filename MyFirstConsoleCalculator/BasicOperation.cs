bbf943035cc9180f171cdfebf8b6721be3480017
Samuel Adedeji <samuel.adedeji@decagon.dev>
2022-05-20 18:13:22 +0100

    My first commit

---
+++ MyFirstConsoleCalculator/BasicOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyFirstConsoleCalculator
+{
+    public class BasicOperation
+    {
+        public static double Addition(double num1, double num2)
+        {
+           double result = num1 + num2;
+            return result;
+        }
+        public double Subtraction(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return result;
+        }
+        public double Multiplication(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return result;
+        }
+        public double Division(double num1, double num2)
+        {
+            
+            double result = num1 / num2;
+            return result;
+        }
+        public double Modulo(double num1, double num2)
+        {
+            double result = num1 % num2;
+            return result;
+        }
+        public double Exp(int num1, int num2)
+        {
+            double result = num1 ^ num2;
+            return result;
+        }
+    }
+}
+    
---
+++ MyFirstConsoleCalculator/OtherOperation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace MyFirstConsoleCalculator
+{
+    public class OtherOperation
+    {
+         public double Exponential(double number)
+        {
+            double result = Math.Exp(number);
+            return result;
+        }
+        public double Square(double number)
+        {
+            double result = Math.Pow(number, 2);
+            return result;
+        }
+
+        public double SquareRoot(double number)
+        {
+            double result = Math.Sqrt(number);
+            return result;
+        }
+
+        public double Cube(double number)
+        {
+            double result = Math.Pow(number, 3);
+            return result;
+        }
+        public double CubeRoot(double number)
+        {
+            double result = Math.Cbrt(number);
+            return result;
+        }
+
+        public double Logarithm(double number)
+        {
+            double result = Math.Log10(number);
+            return result;
+        }
+
+        public double Factorial(double number)
+        {
+
+
+            double result = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+
+            }
+            return result;
+        }
+    }
+}
